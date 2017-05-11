d9191c72b2494dcd7c0e1cb28e522d49f3fe683d
ryersea <ryersea@miamioh.edu>
2017-05-11 12:45:37 -0400

    Add files via upload

---
+++ _387_final_servo_ontheirown.ino
@@ -0,0 +1,41 @@
+
+#include <Servo.h> 
+
+Servo motor; //first servo motor
+Servo motor1; // second servo motor
+
+int state = 0;
+int theta = 1;
+
+void setup() { 
+  Serial.begin(9600);
+  motor.attach(10); //attaches to the second servo motor that moves in the y direction
+  motor1.attach(9); //attaches to the first servo motor that moves in the x direction
+
+ } 
+ 
+void loop() { 
+
+// Checks the position and changes states
+ if (theta <= 1)
+   state = 0;
+    
+ if (theta >= 180)
+  state = 1;
+
+  // Checks the state and moves the position of the servo motor
+ if (state == 0)
+  theta = theta+1;
+    
+ if (state == 1)
+      theta = theta-1;
+
+  // Moves the motor
+  motor.write(theta);  //of the first servo motor
+  delay(15);
+  motor1.write(theta); // of the second servo motor
+
+  Serial.println(theta); 
+
+
+}
---
+++ _387_final_servo_withjoystick.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+Servo tilt; //creates servo object for x axis
+Servo pan; // Creates servo object for y axis
+int joyX = A0; // Analog pin connected to x-axis servo
+int joyY = A1; // Analog pin connected to y-axis servo
+int x; 
+int y; // Variables to read values
+
+void setup() {
+tilt.attach(9); // Attach tilt servo on pin 9 to the servo x object
+pan.attach(10); // Attach pan servo on pin 10 to the servo y object
+
+Serial.begin(9600); //begins serial monitor
+
+}
+
+void loop() {
+x = joyX; // Read value of x-axis (between 0 and 1023)
+y = joyY; // Read value of y-axis (between 0 and 1023)
+x = map(analogRead(joyX), 0, 1023, 900, 2100); // Scale it to use with servo between 900 to 2100 microseconds
+Serial.print("Read x: ");
+Serial.println(x);
+
+y = map(analogRead(joyY), 0, 1023, 900, 2100); 
+Serial.print("Read y: ");
+Serial.println(y);
+tilt.write(x); // Set servo position according to scaled value
+pan.write(y); // sets second servo position according to a scaled value
+delay(15); // Wait for servos to get to new position
+}
